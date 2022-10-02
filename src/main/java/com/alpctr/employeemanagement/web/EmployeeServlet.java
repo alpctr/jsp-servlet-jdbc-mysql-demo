02c9ef9a1b972497fe145521a02fe931af0aac97
alpctr <alpctr@gmail.com>
2022-10-02 17:47:55 +0300

    Add files

---
+++ src/main/java/com/alpctr/employeemanagement/web/EmployeeServlet.java
@@ -0,0 +1,80 @@
+package com.alpctr.employeemanagement.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import com.alpctr.employeemanagement.dao.EmployeeDao;
+import com.alpctr.employeemanagement.model.Employee;
+
+/**
+ * Servlet implementation class EmployeeServlet
+ */
+
+@WebServlet("/register")
+public class EmployeeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private EmployeeDao employeeDao;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployeeServlet() {
+        super();
+        init();
+        // TODO Auto-generated constructor stub
+    	
+
+    }
+
+    public void init() {
+    	employeeDao = new EmployeeDao();
+    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String address = request.getParameter("address");
+		String contact = request.getParameter("contact");
+		
+		Employee employee = new Employee();
+		employee.setFirstName(firstName);
+		employee.setLastName(lastName);
+		employee.setUsername(username);
+		employee.setPassword(password);
+		employee.setContact(contact);
+		employee.setAddress(address);
+		
+		try {
+			employeeDao.registerEmployee(employee);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		response.sendRedirect("employeedetails.jsp");
+		
+		return;
+
+	}
+
+}
